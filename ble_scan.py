20d51d58616c20affffec7b0d9d40e2aafa0cba2
rling123 <82286857+rling123@users.noreply.github.com>
2021-09-15 10:46:14 -0900

    Add files via upload

---
+++ ble_scan.py
@@ -0,0 +1,45 @@
+
+#!/usr/bin/env python3
+"""
+Example of scanning Bluetooth Low Energy (BLE) devices.
+Requires a Linux computer due to gattlib underlying BLE scanning requiring Glib.
+"""
+from __future__ import annotations
+import argparse
+import pprint
+import re
+import logging
+import subprocess
+import argparse
+import datetime
+import json
+import time
+from pathlib import Path
+from bluetooth.ble import DiscoveryService
+
+p = argparse.ArgumentParser(description="BLE scanner")
+p.add_argument(
+    "timeout", help="number of seconds to scan for BLE devices", nargs="?", type=int, default=5
+)
+p.add_argument(
+    'logpath', help="directory to write JSON output to", default="."
+    )
+
+P = p.parse_args()
+
+timeout = P.timeout
+logpath = P.logpath
+
+print(f"Scanning BLE devices for {timeout} seconds")
+
+svc = DiscoveryService()
+ble_devs = svc.discover(timeout)
+
+for u, n in ble_devs.items():
+    
+    print(u, n)
+
+    file1.write(u + n + "\n")
+        
+file1.write("\n")
+    
